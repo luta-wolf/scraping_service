06fce9abab7bdee339ed140964351ad504ea4cae
luta-wolf <denis.skrynnikov.job@gmail.com>
2022-12-25 22:46:55 +0300

    part5 005

--- src/work.py
+++ src/work.py
@@ -1,5 +1,6 @@
 import requests
 import codecs
+from bs4 import BeautifulSoup as BS
 
 '''
 Парсим
@@ -13,28 +14,34 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:47.0) Gecko/20100101 F
 			'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
 			}
 
+
 url = 'https://hh.ru/search/vacancy?search_field=name&search_field=company_name&search_field=description&text=Python+developer'
-url2 = 'https://career.habr.com/vacancies?q=python+developer&l=1&type=all'
+# url2 = 'https://career.habr.com/vacancies?q=python+developer&l=1&type=all'
 resp = requests.get(url, headers=headers)
-h = codecs.open('hh2.html', 'w', 'utf-8')
-h.write(str(resp.text))
+jobs = []
+errors = []
+if resp.status_code == 200:
+	soup = BS(resp.content, 'html.parser')
+	div_lst = soup.find_all('div', attrs={'class': "vacancy-serp-item__layout"})
+	if div_lst:
+		for div in div_lst:
+			title = div.find('a', attrs={'class': 'serp-item__title'})
+			href = title.get('href')
+			vacancy = title.text
+			company = div.find('a').text
+			content = div.find( attrs={'data-qa': 'vacancy-serp__vacancy_snippet_requirement'}).text
+			jobs.append({'title': vacancy, 'url': href, 'description': content, 'company': company,})
+	else:
+		errors.append({'url': url, 'title': 'Div does not exists'})
+else:
+	errors.append({'url': url, 'title': 'Page do not response'})
+
+# for job in jobs:
+# 	print()
+# 	print(*jobs, sep='\n')
+
+h = codecs.open('hh2.txt', 'w', 'utf-8')
+h.write(str(jobs))
 h.close()
 
-'''
-Блок со всеми вакансиями
-<div class="bloko-columns-wrapper">
 
-Блок с одной вакансией
-<div class="vacancy-serp-item__layout">
-
-Блок с названием вакансии и ссылкой на нее
-<a class="serp-item__title" data-qa="serp-item__title" target="_blank" href="https://kazan.hh.ru/vacancy/72814312?from=vacancy_search_catalog&amp;query=Python+developer">Python Developer Trainee</a>
-
-Блок с Названием компании и ссылкой на нее
-<div class="vacancy-serp-item__meta-info-company">
-	<a data-qa="vacancy-serp__vacancy-employer" class="bloko-link bloko-link_kind-tertiary" href="/employer/6093775?hhtmFrom=vacancy_search_catalog">Aston</a>
-</div>
-
-Блок с описанием вакансии
-<div class="g-user-content">
-'''
