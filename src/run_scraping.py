99f6e078b77494a587bd1538356dea32c43ed022
luta-wolf <denis.skrynnikov.job@gmail.com>
2022-12-27 11:32:30 +0300

    part5 011

--- src/run_scraping.py
+++ src/run_scraping.py
@@ -1,10 +1,22 @@
-from  scraping.parsers import *
 import codecs
+import os, sys
+
+proj = os.path.dirname(os.path.abspath('manage.py'))
+sys.path.append(proj)
+os.environ["DJANGO_SETTINGS_MODULE"] = "scraping_service.settings"
+
+import django
+django.setup()
+
+from scraping.parsers import *
+from scraping.models import Vacancy, City, Language
 
 parsers = ((pars_hh, 'https://kazan.hh.ru/search/vacancy?area=113&search_field=name&salary=455000&text=Python+developer&ored_clusters=true&enable_snippets=true'),
 			(pars_habr, 'https://career.habr.com/vacancies?q=python%20developer&s[]=2&s[]=82&s[]=4&type=all'),
 			(pars_job, 'https://russia.superjob.ru/vacancy/search/?keywords=Python%20developer')
 			)
+
+city = City.objects.filter(slug='moskva')
 
 jobs, errors = [], []
 for func, url in parsers:
@@ -12,8 +24,7 @@ for func, url in parsers:
 	jobs += j
 	errors += e
 
-
-h = codecs.open('hh2.txt', 'w', 'utf-8')
+file = codecs.open('hh.txt', 'w', 'utf-8')
 for job in jobs:
-	h.write(str(job) + '\n')
-h.close()
+	file.write(str(job) + '\n')
+file.close()
