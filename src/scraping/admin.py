86027b12cdf3ae173b91c1691f76b49bef99fdb2
luta-wolf <denis.skrynnikov.job@gmail.com>
2023-01-06 16:22:01 +0300

    add list display in Vacancy table

--- src/scraping/admin.py
+++ src/scraping/admin.py
@@ -3,8 +3,14 @@ from . models import City, Language, Vacancy, Error, Url
 
 # Register your models here.
 
+class VacancyAdmin(admin.ModelAdmin):
+	list_display = ('title', 'company', 'language', 'timestamp')
+	list_display_links = ('title',)
+	search_fields = ('title', 'company', 'timestamp')
+
+
 admin.site.register(City)
 admin.site.register(Language)
-admin.site.register(Vacancy)
+admin.site.register(Vacancy, VacancyAdmin)
 admin.site.register(Error)
 admin.site.register(Url)
